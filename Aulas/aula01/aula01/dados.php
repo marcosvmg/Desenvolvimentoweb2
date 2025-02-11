1feaeffdf6f739f4e219a0e1eb0844e39a3eb971
Marcos Martins <61404851+marcosvmg@users.noreply.github.com>
2025-02-11 08:15:21 -0300

    Add files via upload

---
+++ Aulas/aula01/aula01/dados.php
@@ -0,0 +1,7 @@
+<?php
+
+$nome = $_POST['txtnome']; //*com $_GET a informação aparece na URL.
+
+echo "Meu nome é: " . $nome;
+
+?>
---
+++ Aulas/aula01/aula01/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    <form action="dados.php" method="post">
+        <label>Digite seu nome: </label>
+        <input type="text" name="txtnome" placeholder="fifiiiiiiii">
+        <input type="submit" value="Salvar">
+    </form>
+</body>
+</html>
